d0a6bcdc76c084fbdb553c936343398c891def29
DaeHo Kim <rlaeogh222@gmail.com>
2018-11-19 15:25:41 +0900

    first commit
    initialize spring boot project

---
+++ src/main/java/com/dhkim/inflearn_spring_boot/App.java
@@ -0,0 +1,13 @@
+package com.dhkim.inflearn_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+}
